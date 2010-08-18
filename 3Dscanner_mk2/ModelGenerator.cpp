23e85f586c1111bd8ade4267bb1fbdad62462941
MatthewShotton88 <MatthewShotton88@gmail.com>
2010-08-18 14:22:25 +0000

    Have face working and have begun work on adding texture

--- 3Dscanner_mk2/ModelGenerator.cpp
+++ 3Dscanner_mk2/ModelGenerator.cpp
@@ -7,16 +7,19 @@
 
 #include "ModelGenerator.h"
 #include <QDoubleSpinBox>
+#include <QTextStream>
 
 ModelGenerator::ModelGenerator() {
     layout = new QGridLayout(this);
     QDoubleSpinBox *angle_spinbox = new QDoubleSpinBox();
     angle_spinbox->setRange(0, 180);
     angle_spinbox->setSingleStep(0.5);
-    angle_spinbox->setValue(45);
+    angle_spinbox->setValue(35);
     layout->addWidget(new QLabel("Angle of Laser"), 0, 0);
     layout->addWidget(angle_spinbox, 0, 1);
     connect(angle_spinbox, SIGNAL(valueChanged(double)), this, SLOT(setLaserAngle(double)));
+    laserAngle = angle_spinbox->value();
+    params.center_of_rotation = 0.5;
 }
 
 ModelGenerator::~ModelGenerator() {
@@ -42,42 +45,54 @@ void ModelGenerator::generateModel(QVector< QVector<int> > passed, QString passe
     splines = passed;
     filename = passed_name;
     int number_of_rotations = image_manager->number_of_images;
-    int image_width = image_manager->getFirstImage().width();
-    int image_height = image_manager->getFirstImage().height();
+    int image_width = image_manager->getFirstImage().width(); // splines.size()
+    int image_height = image_manager->getFirstImage().height(); //= splines.front().size();
 
     //Generate the texture
-    texture = QImage(number_of_rotations, image_width, QImage::Format_RGB32);
+    texture = QImage(number_of_rotations * 5, image_height, QImage::Format_RGB32);
     for (int x = 0; x < number_of_rotations; x++) {
         QImage tex = image_manager->getTexture(x);
         tex = tex.scaled(image_width, image_height);
         for (int y = 0; y < image_height; y++) {
-            texture.setPixel(x, y, tex.pixel(splines[x][y], y));
+            if ((x * 5) - 2 >= 0)texture.setPixel((x * 5) - 2, y, tex.pixel(splines[x][y] - 1, y));
+            if ((x * 5) - 1 >= 0)texture.setPixel((x * 5) - 1, y, tex.pixel(splines[x][y] - 1, y));
+            texture.setPixel((x * 5), y, tex.pixel(splines[x][y], y));
+            texture.setPixel((x * 5) + 1, y, tex.pixel(splines[x][y] + 1, y));
+            texture.setPixel((x * 5) + 2, y, tex.pixel(splines[x][y] + 2, y));
         }
     }
 
-
+    QString texture_file = filename;
+    texture_file.append(".png");
+    texture.save(texture_file);
     //THINGS TO FIX!!!!
     //add cropping of top and bottom
     //add setting center of rotation
 
+    double spline_mult = cos((double) (90 - (double) laserAngle) / 57.29578); //Account for the angle of the laser
+    //spline_mult = 0.3;
     for (int x = 0; x < number_of_rotations; x++) {
         QVector<point> spl;
         mesh.push_back(spl);
-        double rotation = (360 / (double) number_of_rotations) * x;
+        double rotation = (360 / (double) number_of_rotations) * (double) x;
         for (int y = 0; y < image_height; y++) {
+            double spline_point = ((double) (((double) ((double) image_width * params.center_of_rotation)-(double) splines[x][y])) / spline_mult);
             point po;
-            po.x = (double) cos((double) (rotation / 57.29578))* ((image_width / 2) - splines[x][y]);
+            po.x = (double) cos((double) (rotation / 57.29578)) * spline_point;
             po.y = y;
-            po.z = (double) sin((double) (rotation / 57.29578))* ((image_width / 2) - splines[x][y]);
+            po.z = (double) sin((double) (rotation / 57.29578)) * spline_point;
             //cerr << "point @ " << po.x << " " << po.y << " " << po.z << endl;
             mesh[x].push_back(po);
         }
     }
 
-    QFile test;
-    filename.append(".obj");
-    test.setFileName(filename);
-    test.open(QIODevice::WriteOnly);
+    QFile file;
+    QString object_file = filename;
+    object_file.append(".obj");
+    file.setFileName(object_file);
+    file.open(QIODevice::WriteOnly | QIODevice::Text);
+    QTextStream out(&file);
+
     for (int y = 0; y < image_height; y++) {
         for (int x = 0; x < number_of_rotations; x++) {
 
@@ -93,10 +108,53 @@ void ModelGenerator::generateModel(QVector< QVector<int> > passed, QString passe
             line.append(" ");
             line.append(z_val);
             line.append("\n");
-            cout<<line.toStdString().c_str();
-            test.write(line.toStdString().c_str());
+            out << line.toStdString().c_str();
         }
     }
-    test.close();
+
+
+
+    //cout<<"f v1 v2 v101 v102"<<endl;
+    //cout<<"f v2 v3 v101 v102"<<endl;
+    //cout<<"f v3 v4 v103 v104"<<endl;
+
+    int count = 1;
+    for (int y = 0; y < splines[0].size() - 1; y++) {
+        for (int x = 0; x < splines.size() - 1; x++) {
+            QString line;
+            int x_val = count;
+
+            QString tl = QString(" ").append(QString::number(x_val));
+            QString tr = QString(" ").append(QString::number(x_val + 1));
+            QString bl = QString(" ").append(QString::number(x_val + splines.size()));
+            QString br = QString(" ").append(QString::number(x_val + splines.size() + 1));
+
+            line.append("f ");
+            line.append(tl);
+            line.append(tr);
+            line.append(br);
+            line.append(bl);
+            line.append("\n");
+            out << line.toStdString().c_str();
+            count++;
+        }
+    }
+
+    file.close();
+
+    QString mat_file = filename;
+    mat_file.append(".mtl");
+    QFile mtl;
+    mtl.setFileName(mat_file);
+    QTextStream out2(&mtl);
+    QString mat_file_line = QString("newmtl Textured \nKa 1.000 1.000 1.000\nKd 1.000 1.000 1.000\nKs 0.000 0.000 0.000\nd 1.0\nTr 1.0\nillum 2\nmap_Ka ");
+
+    mat_file_line.append(texture_file);
+    mat_file_line.append("\n map_Kd ");
+    mat_file_line.append(texture_file);
+    out2<<mat_file_line.toStdString().c_str();
+
+    mtl.close();
 
 }
+
